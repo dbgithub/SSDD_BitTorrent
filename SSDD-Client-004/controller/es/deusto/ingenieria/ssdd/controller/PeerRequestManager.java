981063f6f0799991e68b7a08acb5c77e7ed9cde9
Aitor DB <aitor.db@opendeusto.es>
2017-01-03 21:26:09 +0100

    Adding minor comments, nothing special to highlight

--- SSDD-Client-004/controller/es/deusto/ingenieria/ssdd/controller/PeerRequestManager.java
+++ SSDD-Client-004/controller/es/deusto/ingenieria/ssdd/controller/PeerRequestManager.java
@@ -67,48 +67,58 @@ public class PeerRequestManager extends Thread{
 					PeerProtocolMessage message = PeerProtocolMessage.parseMessage(buffer);
 					switch (message.getType()) {
 					case KEEP_ALIVE:
+						// <len=0000>
 						//Message to maintain the connection (due to Timeout)
 						//Do nothing, just renew last message time
 						lastTimeReceivedMessage = new Date();
 						break;
 					case CHOKE:
+						// <len=0001><id=0>
 						//This peers is choked, isn't allowed to request data
 						choked = true;
 						break;
 					case UNCHOKE:
+						// <len=0001><id=1>	
 						//This peer is unchoked, is allowed again to request data
 						choked = false;
 						break;
 					case INTERESTED:
+						// len=0001><id=2>
 						//The other peer is interested, so will request blocks soon
 						//(If it is unchoked)
 						interested = true;
 						break;
 					case NOT_INTERESTED:
+						// <len=0001><id=3>
 						//The other peer isn't interested
 						interested = false;
 						break;
 					case HAVE:
+						// <len=0005><id=4><piece index>
 						//Message when a peer download an piece and is validated 
 						//TODO: Validate Index and drop the connection if it's out of bounds
 						// Send request (INTERESTED) if this peer hasn't that piece
 						break;
 					case BITFIELD:
+						// <len=0001+X><id=5><bitfield>
 						//Sent after the handshake (first message)
 						//TODO: register the pieces that it has and check if we need those
 						//If we need some, send appropriate requests
 						break;
 					case REQUEST:
+						// <len=0013><id=6><index><begin><length>
 						//It is requesting one block, so we have to serve it 
 						//TODO: Look for the piece that wants and send it 
 						break;
 					case PIECE:
-						
+						// <len=0009+X><id=7><index><begin><block>
 						break;
 					case CANCEL:
+						// <len=0013><id=8><index><begin><length>
 						//Cancel download of the block
 						break;
 					case PORT:
+						// <len=0003><id=9><listen-port>
 						//Sets listening port
 						PortMsg portMsg = (PortMsg) message;
 						port = portMsg.getPort();
